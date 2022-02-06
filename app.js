ec2fe03010d59f147c7c69991b1cf5941054d9ef
Muhammed Eroğlu <llofmejb@gmail.com>
2022-02-06 17:30:44 +0300

    2

--- app.js
+++ app.js
@@ -64,3 +64,38 @@ inputText.addEventListener("keyup", function(e){
 // }
 
 // window.onload = getQuote();
+
+const timeArea = document.querySelector(".time");
+const partArea = document.querySelector(".part");
+
+
+setInterval(() => {
+    writeTime()
+}, 1000);
+
+const writeTime = ()=>{
+let currentDate = new Date;
+let seconds = currentDate.getSeconds();
+if (seconds < 10) {
+    seconds = "0"+seconds
+}
+let minutes = currentDate.getMinutes();
+if (minutes < 10) {
+    minutes = "0"+minutes
+}
+let hour = currentDate.getHours();
+if (hour > 12) {
+    hour = hour-12
+    if (hour < 10) {
+        hour = "0"+hour
+    }
+}
+if (hour > 12) {
+    timeArea.innerHTML = hour + ":" + minutes + ":" + seconds;
+    partArea.innerHTML = "PM"
+} else {
+    timeArea.innerHTML = hour + ":" + minutes + ":" + seconds;
+    partArea.innerHTML = "AM"
+    
+}
+}
